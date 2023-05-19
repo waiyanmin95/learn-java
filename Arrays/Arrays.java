03f29f162b542a2d516c8079ac79cf71875978ac
Wai Yan Min <waiyann.min1001@gmail.com>
2023-05-20 00:25:33 +0700

    feat: added lessons

---
+++ Arrays/Arrays.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] xi = {1, 2, 3};
+        System.out.println(xi[1]);
+        System.out.println(xi.length);
+    }
+}
---
+++ Syntax/Syntax.java
@@ -0,0 +1,17 @@
+package Syntax;
+
+public class Syntax {
+    public static void main(String[] args) {
+        int i = 11;
+        long l = 400;
+        char c = 'i';
+        double d = 11.1;
+        float f = 5.6F;
+        boolean b = true;
+        String s = "ivan";
+        System.out.print(s.toUpperCase());
+        System.out.println();
+        System.out.println();
+        System.out.printf("Integer is: %s\nLong is: %s\nFloat is: %s\nBoolean is: %s", i, l, f, b);
+    }
+}
