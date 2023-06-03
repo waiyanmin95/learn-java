31757a0b901b8ea5f5103145d0a2764706f033a5
Wai Yan Min <waiyann.min1001@gmail.com>
2023-06-03 11:53:37 +0700

    feat: map with the lesson names

---
+++ JavaSE/Day5Operators/PrecedenceAssociativity.java
@@ -0,0 +1,31 @@
+package JavaSE.Day5Operators;
+
+public class PrecedenceAssociativity {
+    public static void main(String[] args) {
+        // Precedence == Priority in importance, order, or rank.
+        // Raw Expression
+        // Sort by Precedence
+        // / *
+        // +
+        double result = 10 + 20 / 2 * 4;
+        System.out.println("( result = 10 + 20 / 2 * 4; )");
+        System.out.println("Result Left to Right = " + result);
+        System.out.println("---------------------------------------------------");
+        System.out.println("---------------------------------------------------");
+
+        // Associativity
+        // Operation => Left to Right
+        // Assignment => Right to Left
+        result = 10 + 20 * 2 / 4;
+        System.out.println("( result = 10 + 20 * 2 / 4; )");
+        System.out.println("Result Left to Right = " + result);
+        System.out.println("---------------------------------------------------");
+        System.out.println("---------------------------------------------------");
+
+        // Using Parentheses
+        System.out.println("---------------USING Parentheses ()----------------");
+        result = (10 + 20) * 2 / 4;
+        System.out.println("result = (10 + 20) * 2 / 4;");
+        System.out.println("Result Left to Right = " + result);
+    }
+}
